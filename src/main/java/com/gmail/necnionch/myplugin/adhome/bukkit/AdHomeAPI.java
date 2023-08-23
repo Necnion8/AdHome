3ef3b67a6a85f9ffef2ba6099f21a220c5c55220
Necnion8 <necnionch@gmail.com>
2023-08-23 11:57:10 +0900

    Fix: AdHomeAPIのメソッドDocsを整理

--- src/main/java/com/gmail/necnionch/myplugin/adhome/bukkit/AdHomeAPI.java
+++ src/main/java/com/gmail/necnionch/myplugin/adhome/bukkit/AdHomeAPI.java
@@ -6,129 +6,128 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
-@SuppressWarnings("ALL")
 public class AdHomeAPI {
-    private AdHomePlugin adhome;
+    private final AdHomePlugin adhome;
 
     public AdHomeAPI(AdHomePlugin adhome) {
         this.adhome = adhome;
     }
 
+    /**
+     * 指定されたワールド名に表示名が設定されていれば、その文字列に置換します。
+     * @param worldName ワールド名
+     */
     public String getWorldDisplay(String worldName) {
-        /**
-         * 指定されたワールド名に表示名が設定されていれば、その文字列に置換します。
-         * @param worldName ワールド名
-         */
         return adhome.config.getWorldDisplay(worldName);
     }
 
+    /**
+     * プレイヤーのホームを取得します。
+     * デフォルトホームを取得するには、nameをnullまたは空欄にします。
+     * 存在しない場合は null を返します。
+     * @param player プレイヤー
+     * @param name ホーム名
+     */
     public MyHome getHome(Player player, String name) {
-        /**
-         * プレイヤーのホームを取得します。
-         * デフォルトホームを取得するには、nameをnullまたは空欄にします。
-         * 存在しない場合は null を返します。
-         * @param player プレイヤー
-         * @param name ホーム名
-         */
         return adhome.store.getHome(player, name);
     }
 
+    /**
+     * プレイヤーのホームを取得します。
+     * デフォルトホームを取得するには、nameをnullまたは空欄にします。
+     * 存在しない場合は null を返します。
+     * add: v1.3.0
+     * @param player プレイヤー
+     * @param name ホーム名
+     */
     public MyHome getHome(UUID player, String name) {
-        /**
-         * プレイヤーのホームを取得します。
-         * デフォルトホームを取得するには、nameをnullまたは空欄にします。
-         * 存在しない場合は null を返します。
-         * add: v1.3.0
-         * @param player プレイヤー
-         * @param name ホーム名
-         */
         return adhome.store.getHome(player, name);
     }
 
+    /**
+     * プレイヤーのホームを設定します。
+     * デフォルトホームを設定するには、nameをnullまたは空欄にします。
+     * @param player プレイヤー
+     * @param home ホーム名
+     */
     public void setHome(Player player, MyHome home) {
-        /**
-         * プレイヤーのホームを設定します。
-         * デフォルトホームを設定するには、nameをnullまたは空欄にします。
-         * @param player プレイヤー
-         * @param name ホーム名
-         */
         adhome.store.setHome(player, home);
     }
 
+    /**
+     * プレイヤーのホームを設定します。
+     * デフォルトホームを設定するには、nameをnullまたは空欄にします。
+     * add: v1.3.0
+     * @param player プレイヤー
+     * @param home ホーム
+     */
     public void setHome(UUID player, MyHome home) {
-        /**
-         * プレイヤーのホームを設定します。
-         * デフォルトホームを設定するには、nameをnullまたは空欄にします。
-         * add: v1.3.0
-         * @param player プレイヤー
-         * @param name ホーム名
-         */
         adhome.store.setHome(player, home);
     }
 
+    /**
+     * プレイヤーのホームを削除します。
+     * デフォルトホームを削除するには、nameをnullまたは空欄にします。
+     * @param player プレイヤー
+     * @param home ホーム
+     */
     public void deleteHome(Player player, MyHome home) {
-        /**
-         * プレイヤーのホームを削除します。
-         * デフォルトホームを削除するには、nameをnullまたは空欄にします。
-         * @param player プレイヤー
-         * @param name ホーム名
-         */
         adhome.store.deleteHome(player, home.getName());
     }
 
+    /**
+     * プレイヤーのホームを削除します。
+     * デフォルトホームを削除するには、nameをnullまたは空欄にします。
+     * add: v1.3.0
+     * @param player プレイヤー
+     * @param home ホーム
+     */
     public void deleteHome(UUID player, MyHome home) {
-        /**
-         * プレイヤーのホームを削除します。
-         * デフォルトホームを削除するには、nameをnullまたは空欄にします。
-         * add: v1.3.0
-         * @param player プレイヤー
-         * @param name ホーム名
-         */
         adhome.store.deleteHome(player, home.getName());
     }
 
+    /**
+     * プレイヤーの全てのホームの名前を取得します。
+     * @param player プレイヤー
+     */
     public Set<String> getAllHomeNames(Player player) {
-        /**
-         * プレイヤーの全てのホームの名前を取得します。
-         * @param player プレイヤー
-         */
         return adhome.store.getAllHomeNames(player);
     }
 
+    /**
+     * プレイヤーの全てのホームの名前を取得します。
+     * add: v1.3.0
+     * @param player プレイヤー
+     */
     public Set<String> getAllHomeNames(UUID player) {
-        /**
-         * プレイヤーの全てのホームの名前を取得します。
-         * add: v1.3.0
-         * @param player プレイヤー
-         */
         return adhome.store.getAllHomeNames(player);
     }
 
+    /**
+     * プレイヤーのデフォルトホームを含む全てのホームを取得します。
+     * 名前を取得する場合は、getAllHomeNamesを使用してください。速度面で有利です。
+     * @param player プレイヤー
+     */
     public Set<MyHome> getAllHomes(Player player) {
-        /**
-         * プレイヤーのデフォルトホームを含む全てのホームを取得します。
-         * 名前を取得する場合は、getAllHomeNamesを使用してください。速度面で有利です。
-         * @param player プレイヤー
-         */
         return new HashSet<>(adhome.store.getAllHomes(player));
     }
 
+    /**
+     * プレイヤーのデフォルトホームを含む全てのホームを取得します。
+     * 名前を取得する場合は、getAllHomeNamesを使用してください。速度面で有利です。
+     * add: v1.3.0
+     * @param player プレイヤー
+     */
     public Set<MyHome> getAllHomes(UUID player) {
-        /**
-         * プレイヤーのデフォルトホームを含む全てのホームを取得します。
-         * 名前を取得する場合は、getAllHomeNamesを使用してください。速度面で有利です。
-         * add: v1.3.0
-         * @param player プレイヤー
-         */
         return new HashSet<>(adhome.store.getAllHomes(player));
     }
 
+    /**
+     * 全プレイヤーのUUIDを取得します
+     * 読み込みに失敗したエントリは無視します
+     * add: v1.3.0
+     */
     public UUID[] getUsers() {
-        /**
-         * 全プレイヤーのUUIDを取得します
-         * 読み込みに失敗したエントリは無視します
-         * add: v1.3.0
-         */
         return adhome.store.getUsers();
     }
 
